412d5b7d4bec8713f7b549cdb74031d1392de4c9
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-11-09 14:34:00 +0300

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -13,8 +13,6 @@ async function register(req, res) {
       return res.status(400).json({ msg: 'Email already used' });
     }
 
-
-
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({ email, passwordHash: hash, name });
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
